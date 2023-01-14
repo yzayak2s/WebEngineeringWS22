1153bf3f6bc241018b6b3021fc9f3ac8e446821b
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-01-14 12:36:41 +0100

    implemented getTableData()-method

--- pages/uebung5/topsort-web-app.js
+++ pages/uebung5/topsort-web-app.js
@@ -18,3 +18,28 @@ function removeRow() {
         table.deleteRow(-1)
     }
 }
+
+function getTableData() {
+    const table = document.querySelector('table')
+
+    const myArray = [];
+
+    // Anzahl der 'tr' im 'tbody'
+    let rowLength = table.tBodies.item(0).rows.length
+    for (let i = 0; i < rowLength; i++) {
+
+        let currentCells = table.tBodies.item(0).rows.item(i).cells;
+
+        // Anzahl der 'td' im 'tr'
+        let cellLength = currentCells.length
+
+        let myTempArray = [];
+
+        for (let j = 0; j < cellLength; j++) {
+            myTempArray.push(currentCells.item(j).innerHTML);
+        }
+        myArray.push(myTempArray)
+
+    }
+    return myArray;
+}
