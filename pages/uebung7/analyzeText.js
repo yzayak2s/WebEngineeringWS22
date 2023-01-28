a0ab6d51d67a02b89bb2385a6ebb3f58b4f1b7ad
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-01-28 13:29:05 +0100

    outsourced analyzeText function and some refactoring

--- pages/uebung7/analyzeText.js
+++ pages/uebung7/analyzeText.js
@@ -13,8 +13,65 @@
  *      - installiere @babel/core, @babel/cli und @babel/preset-env
  *      - erstelle im root Verzeichnis eine config Datei 'babel.config.json'
  *      - ...
+ *
+ *  Der einfachste Ansatz ist 'importmap' zu nutzen!
  */
-function analyzeText(text) {
-    console.log(stopwords)
+
+import stopwords from "stopwords" assert {type: "json"};
+
+const STOPWORDS = stopwords
+
+function textAnalyze() {
+    fetch('https://kaul.inf.h-brs.de/ccm/we/ws22/resources/assets/Plagiatsresolution.html')
+        .then((response) => response.text())
+        .then((responseWithHTML) => removeHTML(responseWithHTML))
+        .then((responseWithoutHTML) => {
+            let wordCounts = {};
+
+            // remove all line breaks, commas, dots, hyphens, braces and filtered white spaces out
+            // Reference for regEx: https://regex101.com/
+            const words = responseWithoutHTML.toLowerCase().replaceAll(/["().,-]|\n/gm, '').split(' ').filter((value) => value !== '')
+
+            // Reference: https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array/41169035#41169035
+            const filteredWords = words.filter((word) => !STOPWORDS.includes(word))
+
+            // Count most common words in string
+            // Reference: https://stackoverflow.com/questions/6565333/using-javascript-to-find-most-common-words-in-string/6565353#6565353
+            for (let i = 0; i < filteredWords.length; i++) {
+                wordCounts[filteredWords[i]] = (wordCounts[filteredWords[i]] || 0) + 1;
+            }
+
+            // Descended sorting
+            // Reference: https://stackoverflow.com/questions/3524827/sort-a-2d-array-by-the-second-value/3524832#3524832
+            const sortedWords = Object.entries(wordCounts).sort((a, b) => {
+                return b[1] - a[1];
+            })
+
+            // Get 3 first elements in an array
+            // Reference: https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array/34883171#34883171
+            console.log(sortedWords.slice(0, 3))
+            return sortedWords.slice(0, 3)
+
+            // Alternative solution for counting frequency in a string
+            // Reference: https://stackoverflow.com/questions/18619785/counting-frequency-of-characters-in-a-string-using-javascript/42636979#42636979
+            /*filteredWords.reduce((total, word) => {
+                total[word] ? total[word]++ : total[word] = 1;
+                return total;
+            }, {})*/
+
+        });
+}
+
+window.textAnalyze = textAnalyze
+
+/**
+ * @author
+ * Reference: https://stackoverflow.com/questions/822452/strip-html-from-text-javascript/47140708#47140708
+ * @param html
+ * @return {string|string}
+ */
+function removeHTML(html) {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
 
 }
