f91b5f40f430e16877b8f5110ee2aee0a09087ec
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-01-28 12:06:31 +0100

    provided highlighting clicked buttons and corresponding css style properties

---
+++ pages/uebung8/script.js
@@ -0,0 +1,19 @@
+// Keep clicked button highlighted
+// Reference: https://stackoverflow.com/questions/68347721/how-do-i-keep-a-clicked-navigation-button-highlighted-in-css/68348062#68348062
+let elements = document.querySelectorAll('.header-nav-button')
+
+elements.forEach(el => {
+    el.addEventListener('click', () => {
+        elements.forEach(el => el.classList.remove('active'))
+        el.classList.add('active')
+    })
+})
+
+let elementsBtn = document.querySelectorAll('.main-nav-button')
+
+elementsBtn.forEach(el => {
+    el.addEventListener('click', () => {
+        elementsBtn.forEach(el => el.classList.remove('active'))
+        el.classList.add('active')
+    })
+})
